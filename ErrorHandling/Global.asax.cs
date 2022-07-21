434458c1ae3f2ef15d55a858e292397a111ea2e9
HP <HP@DESKTOP-G4J7E8F>
2022-07-21 17:37:01 +0530

    Add project files.

---
+++ ErrorHandling/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace ErrorHandling
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+            Server.Transfer("demo.aspx");
+            Server.ClearError();
+        }
+    }
+}
---
+++ ErrorHandling/Index.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ErrorHandling
+{
+    public partial class index : System.Web.UI.Page
+    {
+        DataSet ds;
+        SqlDataAdapter da;
+        SqlConnection conn;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["mcon"].ConnectionString);
+            da = new SqlDataAdapter("select * from mydata", conn);
+            ds = new DataSet();
+            da.Fill(ds,"Emp");
+            GridView1.DataSource = ds.Tables["Emp"];
+            GridView1.DataBind();
+
+        }
+        ////page level error handling
+        protected void Page_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+            Server.Transfer("demo.aspx");
+            Server.ClearError();
+        }
+    }
+}
